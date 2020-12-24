6114f4eb8d1eb57244ae19ac284b47bdb68dab1f
David <charlesinvestingltd@gmail.com>
2020-12-23 19:24:44 -0500

    started on the coins controller

---
+++ app/controllers/api/v1/coins_controller.rb
@@ -0,0 +1,42 @@
+module Api
+  module V1
+    class CoinsController < ApplicationController
+      def index
+        coins = Coin.all
+
+        render json: CoinSerializer.new(coins).serialized_json
+      end
+
+      def show
+        coin = Coin.find_by(slug: params[:slug])
+
+        render json: CoinSerializer.new(coin).serialized_json
+      end
+
+      def create
+        coin = Coin.new(coin_params)
+        if coin.save
+          render json: CoinSerializer.new(coin).serialized_json
+        else
+          render json: {error: coin.error.messages}, status: 422
+        end
+      end
+
+      def update
+        coin = Coin.find_by(slug: params[:slug])
+        if coin.update(coin_params)
+          render json: CoinSerializer.new(coin).serialized_json
+        else
+          render json: {error: coin.error.messages}, status: 422
+        end
+      end
+
+
+      private
+
+      def coin_params
+        params.require(:coin).permit(:name, :image_url)
+      end
+    end
+  end
+end
